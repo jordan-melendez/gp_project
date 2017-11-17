8a0b47c24ba5d13a00e6e99ee04b401671dbdb3c
JHonaker <honaker215@gmail.com>
2017-11-16 20:25:34 -0500

    Added support for constraints on the parameter space.

--- gp_project/hamMCMC.py
+++ gp_project/hamMCMC.py
@@ -3,6 +3,18 @@ import numpy as np
 class HamiltonianSampler:
 
     def __init__(self, U, grad_U, num_leaps, step_size):
+        """Initializes the main sampler class
+
+        MUST CALL initialize to set starting point before running algorithm
+
+        Parameters:
+        --------------------
+        U: A function that evaluates the exponent of an exponential family distribution
+        grad_U: A function that evaluates the gradient of U
+        num_leaps: The number of leapfrog iterations to do per sample round
+        step_size: Epsilon for size of leapfrog iterations
+
+        """
         self.U = U
         self.grad_U = grad_U
 
@@ -17,9 +29,28 @@ class HamiltonianSampler:
         np.random.seed(seed)
 
     def initialize(self, start_q):
+        """Sets the starting point for the MCMC sampler, and initializes some important variables"""
         self.num_dimensions = len(start_q)
 
+        self.upper_bounds = np.full([self.num_dimensions], np.Infinity)
+        self.lower_bounds = np.full([self.num_dimensions], -np.Infinity)
+
         self.current_position = start_q
+
+    def set_bounds(self, bound_dict):
+        """Setter for the upper and lower bounds of the parameter spaces.
+        
+        Parameters
+        ----------
+        bound_dict: A dictionary where the keys are the parameter vector index;
+                    its values are a 2-tuple with (lower, upper) bounds.
+                    
+                    Example: { 3: (0, np.Infinity) } to constrain to X >= 0"""
+
+        for idx in bound_dict:
+            lower, upper = bound_dict[idx]
+            self.lower_bounds[idx] = lower
+            self.upper_bounds[idx] = upper
 
     def kinetic_energy(self, velocity):
         return np.sum(np.square(velocity)) / 2
@@ -28,7 +59,15 @@ class HamiltonianSampler:
         return (self.U(position), self.kinetic_energy(velocity))
 
     def _update_position(self, position, velocity, step_size):
-        return position + step_size * velocity
+        new_position = position + step_size * velocity
+
+        above_bound = new_position > self.upper_bounds
+        below_bound = new_position < self.lower_bounds
+        
+        new_position[above_bound] = (2 * self.upper_bounds - new_position)[above_bound]
+        new_position[below_bound] = (2 * self.lower_bounds - new_position)[below_bound]
+
+        return new_position
 
     def _update_velocity(self, position, velocity, step_size):
         return velocity - step_size * self.grad_U(position)
