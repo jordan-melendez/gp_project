a75b6d00ae8dcb2f5068b55995eb0666da2513d1
JHonaker <honaker215@gmail.com>
2017-11-29 21:05:53 -0500

    Finished the gradient of U without R inverse.

--- gp_project/model.py
+++ gp_project/model.py
@@ -21,7 +21,6 @@ def UFactory(delta, Xs, rhos, mu_mu, sigmasq_mu, alpha_Q, beta_Q, alpha_sig, bet
         V = delta - mu / (1 - Q)
 
         return -N/2 * np.log(sigsq) \
-               - 1/2 * logDetR(position) \
                - N/2 * Q^(2*K + 2) / (1 - Q^2) \
                - 1/(2*sigsq) * V.T @ Rinv @ V * (1-Q^2) / Q^(2*K + 2)
 
@@ -41,7 +40,7 @@ def UFactory(delta, Xs, rhos, mu_mu, sigmasq_mu, alpha_Q, beta_Q, alpha_sig, bet
 
     def logQ(position):
         Q = position[index["Q"]]
-        return (alpha - 1) * np.log(Q) + (beta - 1) * np.log(1 - Q)
+        return (alpha_Q - 1) * np.log(Q) + (beta_Q - 1) * np.log(1 - Q)
 
     def logMu(position):
         mu = position[index["MU"]]
@@ -62,45 +61,94 @@ def UFactory(delta, Xs, rhos, mu_mu, sigmasq_mu, alpha_Q, beta_Q, alpha_sig, bet
 
 def GradUFactory(delta, Xs, rhos, mu_mu, sigmasq_mu, alpha_sig, beta_sig, alpha_Q, beta_Q):
 
-    def Rinverse(position):
-        raise NotImplementedError
+    N, K = Xs.shape
+
+    Rinv = None # Doesn't depend on anything by the Xs and the rhos, so its constant wrt Q, sigsq, mu
+                # LogDetR doesn't need calculated since its constant and cancels in MH step
 
     def dDeltadSigmasq(position):
-        raise NotImplementedError
+        Q = position[index["Q"]]
+        sigmasq = position[index["SIGMASQ"]]
+        mu = position[index["MU"]] * np.ones(N)
+
+        return -N/2 * 1/sigmasq + 1/2 * 1/sigmasq^2 * V @ Rinv @ V * (1 - Q^2) / Q^(2*(K+2 + 1))
 
     def dDeltadMu(position):
-        raise NotImplementedError
+        Q = position[index["Q"]]
+        sigmasq = position[index["SIGMASQ"]]
+        mu = position[index["MU"]] * np.ones(N)
+
+        V = delta - mu / (1-Q)
+
+        # TODO Double check the value K, K+2, etc.
+        return 1/sigmasq * Rinv @ V * 1/(1-Q)^2 * (1-Q^2)/Q^(2*(K+2 + 1)
 
     def dDeltadQ(position):
-        raise NotImplementedError
+        Q = position[index["Q"]]
+        sigmasq = position[index["SIGMASQ"]]
+        mu = position[index["MU"]] * np.ones(N)
 
-    def dCidSigmasq(position):
-        raise NotImplementedError
+        k = K + 2
 
-    def dCidMu(position):
-        raise NotImplementedError
+        V = delta - mu / (1-Q)
 
-    def dCidQ(position):
-        raise NotImplementedError
+        first = (1-Q^2)/Q^(2*k+2) * (2 * (k+1) * Q^(2*k + 1) * (1-Q^2) + 2*Q^(2*k + 3)) / (1 - Q^2)^2
+        second = 1/sigmasq * Rinv @ V @ np.ones(N) * 1 / (1-Q)^2 * (1 - Q^2) / Q^(2*k + 2)
+        third = 1/sigmasq * V.T @ Rinv @ V * (Q^(2*k + 3) - (k+1) * Q^(2*k + 1) * (1 - Q^2)) / Q^(4*k + 4)
+
+        return first + second + third
+
+
+    def dCidSigmasq(position, i):
+        Q = position[index["Q"]]
+        sigmasq = position[index["SIGMASQ"]]
+        mu = position[index["MU"]] * np.ones(N)
+        Ci = Xs[:, i] / Q^(i+2)
+
+        V = Ci - mu
+
+        return -N/2 * 1/sigmasq + 1/2 * 1/sigmasq^2 * V.T @ Rinv @ V
+
+    def dCidMu(position, i):
+        Q = position[index["Q"]]
+        sigmasq = position[index["SIGMASQ"]]
+        mu = position[index["MU"]] * np.ones(N)
+        Ci = Xs[:, i] / Q^(i+2)
+
+        V = Ci - mu
+
+        return 1/sigmasq * Rinv @ V @ np.ones(N)
+
+    def dCidQ(position, i):
+        Q = position[index["Q"]]
+        sigmasq = position[index["SIGMASQ"]]
+        mu = position[index["MU"]] * np.ones(N)
+        Ci = Xs[:, i] / Q^(i+2)
+
+        V = Ci - mu
+
+        return 1 / sigmasq * Rinv @ V @ Ci/Q * (i+2)
 
     def dCdSigmasq(position):
-        raise NotImplementedError
+        return sum(dCidSigmasq(position, i) for i in range(K))
 
     def dCdMu(position):
-        raise NotImplementedError
+        return sum(dCidMu(position, i) for i in range(K))
 
     def dCdQ(position):
-        raise NotImplementedError
+        return sum(dCidQ(position, i) for i in range(K))
 
     def dlogQdQ(position):
         Q = position[index["Q"]]
-        return (alpha - 1) / Q - (beta - 1) / (1 - Q)
+        return (alpha_Q - 1) / Q - (beta_Q - 1) / (1 - Q)
 
     def dlogMudMu(position):
-        raise NotImplementedError
+        mu = position[index["MU"]]
+        return - (mu - mu_mu)
 
     def dlogSigmasqdSigmaSq(position):
-        raise NotImplementedError
+        sigmasq = position[index["SIGMSQ"]]
+        return -(alpha_sig - 1) / sigmasq + beta_sig / sigmasq^2
 
     def dJointdQ(position):
         return dDeltadQ(position) + dCdQ(position) + dlogQdQ(position)
